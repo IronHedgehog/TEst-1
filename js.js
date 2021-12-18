c89f27b36a1fe9a9a24df51cf43731a304cd3832
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2021-12-19 01:07:45 +0200

    test

---
+++ js.js
@@ -0,0 +1,8 @@
+const linkAlert = document.querySelector('.js-checkbox')
+
+linkAlert.addEventListener('click', () => {
+    alert('https://meet.google.com/reo-mdfs-ubf')
+    linkAlert.removeClass('checked')
+})
+
+// alert("hello")
